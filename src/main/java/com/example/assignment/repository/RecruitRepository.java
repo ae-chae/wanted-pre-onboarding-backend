436481dc10268b903a95139fcf3695d96dae825a
ae-chae <aechae11@gmail.com>
2024-08-05 14:17:17 +0900

    [Feat] SEARCH API

--- src/main/java/com/example/assignment/repository/RecruitRepository.java
+++ src/main/java/com/example/assignment/repository/RecruitRepository.java
@@ -4,7 +4,10 @@ import com.example.assignment.entity.Recruit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecruitRepository extends JpaRepository<Recruit, Integer> {
-
+    List<Recruit> findByNumOrCompanyIdContainingOrRewardOrTechContainingOrDistrictContainingOrPositionContaining(
+            int num, String companyId, Long reward, String tech, String district, String position);
 }
